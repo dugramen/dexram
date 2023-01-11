70a4ab5c36939bd862c9f2e7065022cc95ee98ff
krahman <koliur.rahman@my.liu.edu>
2023-01-11 09:27:50 -0500

    Initial commit, working search & type filter

---
+++ Components/Utils.tsx
@@ -0,0 +1,17 @@
+export async function cacheFetch(request: RequestInfo | URL, options?: any) {
+    const cache = await caches.open('pokeapi');
+    const response = await cache.match(request)
+    if (response !== undefined) {
+        console.log('existing request ', request)
+        return response
+    }
+    else {
+        console.log('new request ', request)
+        await cache.add(request)
+        return cache.match(request)
+    }
+}
+
+export const capitalize = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+} 
