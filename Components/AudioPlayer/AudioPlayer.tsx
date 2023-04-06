09a60b092c186e5f6822cb136f72c2002e8420cb
krahman <koliur.rahman@my.liu.edu>
2023-04-06 13:52:46 -0400

    Fixed playback issues

--- Components/AudioPlayer/AudioPlayer.tsx
+++ Components/AudioPlayer/AudioPlayer.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {faPlay, faPause} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 // Reverse an AudioBuffer
 function reverseBuffer(audioBuffer) {
@@ -47,6 +50,15 @@ function convolve(source: AudioBufferSourceNode, audioContext: AudioContext, wav
     convolverNode.connect(audioContext.destination);
 }
 
+function dampen(source: AudioBufferSourceNode, context: AudioContext, Q = 1, cutoff = 1000) {
+    const filter = context.createBiquadFilter();
+    filter.type = 'lowpass';
+    filter.frequency.value = cutoff;
+    filter.Q.value = Q;
+    source.connect(filter)
+    filter.connect(context.destination)
+}
+
 const cloneBuffer = (original: AudioBuffer, context: AudioContext) => {
     const { numberOfChannels, length, sampleRate } = original
     const clone = context.createBuffer(numberOfChannels, length, sampleRate);
@@ -57,35 +69,86 @@ const cloneBuffer = (original: AudioBuffer, context: AudioContext) => {
     return clone
 }
 
-export default function AudioPlayer (p: {url: string, options: {pitch, reverse, impulseWave}}) {
+export default function AudioPlayer (p: {url: string, options: {loop, pitch, reverse, impulseWave}, setStopper?: (stopper: () => any) => any}) {
     const [context, setContext] = React.useState(new AudioContext())
     const [audioBuffer, setAudioBuffer] = React.useState<AudioBuffer>()
+    const [isPlaying, setIsPlaying] = React.useState(false)
+    const innerReverse = React.useRef(false)
 
     const {options} = p
+    const {loop, pitch, reverse, impulseWave} = options
     const sourceRef = React.useRef<AudioBufferSourceNode>()
     
-
-    function play() {
-        if (!audioBuffer) { return }
+    function stopAudio() {
+        console.log('stopped')
         try {
-            sourceRef.current?.stop()
-        } catch {
-            console.log('no other audio is currently playing')
-        }
+            sourceRef.current?.disconnect()
+            // sourceRef.current?.stop()
+            context.suspend()
+        } catch {console.log}
+        setIsPlaying(false)
+    }
+    React.useEffect(() => {
+        p.setStopper?.(() => {
+            return stopAudio
+        })
+    }, [p.setStopper])
+    
+    const applyAudioMods = (force?) => {
+        if (!sourceRef.current) {return}
+        const source = sourceRef.current
+        try {
+            source.disconnect()
+            source.connect(context.destination)
+        } catch { console.log }
+        options.pitch !== undefined && changePitch(source, context, parseInt(options.pitch));
+        ((force && options.reverse) || (options.reverse !== innerReverse.current)) && reverseBuffer(source.buffer);
+        innerReverse.current = options.reverse
+        dampen(source, context, .001, 0)
+        options.impulseWave && convolve(source, context, options.impulseWave)
+        source.loop = options.loop
+    }
+    function play() {
+        // stopAudio()
+        if (!audioBuffer) { return }
 
+        // const old = sourceRef.current
         const source = context.createBufferSource()
         sourceRef.current = source
         source.buffer = cloneBuffer(audioBuffer, context)
-
-        options.pitch !== undefined && changePitch(source, context, options.pitch);
-        options.reverse && reverseBuffer(source.buffer);
-        options.impulseWave && convolve(source, context, options.impulseWave)
-
+        
+        applyAudioMods(true)
+        console.log('began ')
+        source.onended = () => {
+            setTimeout(() => {
+                console.log('ended')
+                setIsPlaying(false)
+            }, 100)
+        }
         source.connect(context.destination)
-        source.start()
+        
+        try {
+            context.resume()
+        } catch {console.log}
+        
+        source.start(0)
+        setIsPlaying(true)
+        
     }
 
+    // React.useEffect(() => {
+    //     console.log('playing ', isPlaying)
+    // }, [isPlaying])
+    
     React.useEffect(() => {
+        console.log('options ', options)
+        applyAudioMods()
+    }, [options])
+
+    
+
+    React.useEffect(() => {
+        stopAudio()
         fetch(`/api/audio?url=${encodeURIComponent(p.url)}`)
         .then(res => res.arrayBuffer())
         .then(res => context.decodeAudioData(res))
@@ -101,8 +164,13 @@ export default function AudioPlayer (p: {url: string, options: {pitch, reverse, 
 
     return (
         <div>
-            <button onClick={play}>
-                Play
+            <button 
+                onClick={isPlaying ? stopAudio : play}
+
+            >
+                <FontAwesomeIcon
+                    icon={isPlaying ? faPause : faPlay}
+                />
             </button>
         </div>
     )
--- Components/InfoPanel/SoundPanel.tsx
+++ Components/InfoPanel/SoundPanel.tsx
@@ -13,6 +13,7 @@ export default function SoundPanel(props: {id}) {
         impulseWave: '',
     })
     const [impulseWaves, setImpulseWaves] = React.useState([])
+    const [stopper, setStopper] = React.useState<() => any>()
     const audioRef = React.useRef<any>(null)
     // const [context, setContext] = React.useState(new AudioContext())
 
@@ -38,9 +39,14 @@ export default function SoundPanel(props: {id}) {
                 url={`https://play.pokemonshowdown.com/audio/cries/${pkData[props.id]?.pokemon?.identifier}.mp3`}
                 // convolver={true}
                 options={options}
+                setStopper={setStopper}
                 // url="https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3"
                 // pitch={15}
             />
+
+            <button onClick={() => stopper?.()}>
+                Stop
+            </button>
 
             <select 
                 onChange={e => setOptions(old => ({...old, impulseWave: e.target.value})) }
@@ -50,17 +56,17 @@ export default function SoundPanel(props: {id}) {
                 {impulseWaves.map(wave => <option key={wave} value={wave}>{wave}</option>)}
             </select>
 
-            <audio
+            {/* <audio
                 controls
                 src={`https://play.pokemonshowdown.com/audio/cries/${pkData[props.id]?.pokemon?.identifier}.mp3`}
                 loop={loop}
                 ref={audioRef}
-            />
+            /> */}
 
             <label> 
                 <input
                     type={'checkbox'}
-                    checked={loop}
+                    checked={options.loop}
                     onChange={e => setOptions(old => ({...old, loop: e.target.checked}))}
                 ></input>
                 Loop? 
@@ -75,14 +81,18 @@ export default function SoundPanel(props: {id}) {
                 Reverse? 
             </label>
 
+            <label>{options.pitch}</label>
             <input
                 type='range'
-                min={0.5}
-                max={2.0}
-                step={0.1}
+                min={-25}
+                max={25}
+                step={1}
                 
-                value={playbackRate}
-                onChange={event => setPlaybackRate(parseFloat(event.target.value))}
+                value={options.pitch}
+                onChange={event => setOptions(old => ({
+                    ...old,
+                    pitch: parseInt(event.target.value)
+                }))}
             />
         </div>
     )
